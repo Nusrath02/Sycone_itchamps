5f107098d78681b1a5cf65a48956b2d10c8b5e7b
Nusrath02 <nusrath.s@itchamps.com>
2025-09-19 12:09:44 +0530

    Update hooks.py

--- business_theme_v14/hooks.py
+++ business_theme_v14/hooks.py
@@ -30,8 +30,7 @@ app_include_js = "/assets/business_theme_v14/js/sycone_footer.js"
 
 
 
-# app_include_js = "/assets/business_theme_v14/js/business_theme_v14.js"
-app_include_js = "/assets/business_theme_v14/js/Horizontal.js"
+
 # Favicon
 # Website settings
 website_context = {
