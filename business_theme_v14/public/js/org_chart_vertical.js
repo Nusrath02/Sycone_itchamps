3418cf240cc18a3e4f05a9a13275aa89a3c55a0b
Nusrath02 <nusrath.s@itchamps.com>
2026-01-07 10:28:32 +0530

    Update org_chart_vertical.js

--- business_theme_v14/public/js/org_chart_vertical.js
+++ business_theme_v14/public/js/org_chart_vertical.js
@@ -1,55 +1,75 @@
+frappe.pages["vertical-org-chart"] =
+    frappe.pages["vertical-org-chart"] || {};
+
 frappe.pages["vertical-org-chart"].on_page_load = function (wrapper) {
 
     const page = frappe.ui.make_app_page({
         parent: wrapper,
-        title: "Vertical Org Chart",
+        title: "Custom Vertical Org Chart",
         single_column: true
     });
 
-    const container = $('<div id="custom-org"></div>');
+    const container = $('<div class="custom-org-wrapper"></div>');
     $(page.body).append(container);
 
     frappe.call({
         method: "frappe.client.get_list",
         args: {
             doctype: "Employee",
-            fields: ["name", "employee_name", "designation", "reports_to"],
+            fields: [
+                "name",
+                "employee_name",
+                "designation",
+                "reports_to"
+            ],
             limit_page_length: 1000
         },
         callback(r) {
+            if (!r.message) return;
+
             const tree = buildHierarchy(r.message);
-            renderVertical(tree, container);
+            renderTree(tree, container);
         }
     });
 
 };
 
+
 function buildHierarchy(list) {
     const map = {};
     const roots = [];
 
-    list.forEach(e => map[e.name] = { ...e, children: [] });
+    list.forEach(emp => {
+        map[emp.name] = { ...emp, children: [] };
+    });
 
-    list.forEach(e => {
-        if (e.reports_to && map[e.reports_to]) {
-            map[e.reports_to].children.push(map[e.name]);
+    list.forEach(emp => {
+        if (emp.reports_to && map[emp.reports_to]) {
+            map[emp.reports_to].children.push(map[emp.name]);
         } else {
-            roots.push(map[e.name]);
+            roots.push(map[emp.name]);
         }
     });
 
     return roots;
 }
 
-function renderVertical(nodes, parent) {
+
+function renderTree(nodes, parent) {
     const ul = $('<ul class="org-vertical"></ul>');
 
-    nodes.forEach(n => {
+    nodes.forEach(node => {
         const li = $('<li></li>');
-        li.append(`<div class="node">${n.employee_name}<br>${n.designation}</div>`);
 
-        if (n.children.length) {
-            renderVertical(n.children, li);
+        li.append(`
+            <div class="org-node">
+                <div class="emp-name">${node.employee_name}</div>
+                <div class="emp-desig">${node.designation || ""}</div>
+            </div>
+        `);
+
+        if (node.children.length) {
+            renderTree(node.children, li);
         }
 
         ul.append(li);
